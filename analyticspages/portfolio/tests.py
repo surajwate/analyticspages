b555a7a483413e15e131b859d987d63f795dc593
Suraj Wate <suraj.wate@gmail.com>
2024-06-22 17:42:21 +0100

    refactored the code

--- analyticspages/portfolio/tests.py
+++ analyticspages/portfolio/tests.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.contrib.admin.sites import AdminSite
 from .admin import ProjectAdmin
 
+# Test for creating a project instance and verifying its attributes
 @pytest.mark.django_db
 def test_project_create():
     project = Project.objects.create(
@@ -16,6 +17,7 @@ def test_project_create():
     assert project.description == 'Test Description'
     assert project.technology == 'Test Technology'
 
+# Test for the project_list view to ensure that it returns a 200 status code and contains 'Projects' in the response content
 @pytest.mark.django_db
 def test_project_list_view(client):
     url = reverse('project_list')
@@ -23,14 +25,17 @@ def test_project_list_view(client):
     assert response.status_code == 200
     assert 'Projects' in response.content.decode()
 
+# `admin_site` fixture creates an instance of the `AdminSite` class
 @pytest.fixture
-def admin_site():                   # `admin_site` fixture creates an instance of the `AdminSite` class
+def admin_site():                   
     return AdminSite()
 
+# `project_admin` fixture creates an instance of the `ProjectAdmin` class using the `admin_site` fixture
 @pytest.fixture
-def project_admin(admin_site):      # `project_admin` fixture creates an instance of the `ProjectAdmin` class using the `admin_site` fixture
+def project_admin(admin_site):      
     return ProjectAdmin(Project, admin_site)
 
+# Test to check that the `list_display` attribute of the `ProjectAdmin` class is set correctly
 @pytest.mark.django_db
-def test_project_admin_list_display(project_admin):         # This test checks that the `list_display` attribute of the `ProjectAdmin` class is set correctly
+def test_project_admin_list_display(project_admin):         
     assert project_admin.list_display == ('title', 'technology', 'description')
