1c80f20e140e3532447579248770b869b3135c02
Suraj Wate <suraj.wate@gmail.com>
2024-06-22 21:33:18 +0100

    added comments for better explanation

--- analyticspages/portfolio/views.py
+++ analyticspages/portfolio/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Project
 
+# Function-based view to display a list of all projects
 def project_list(request):
     projects = Project.objects.all()
     return render(request, 'portfolio/project_list.html', {'projects': projects})
 
+# Function-based view to display the details of a specific project
 def project_list(request, pk):
     projects = get_object_or_404(Project, pk=pk)
     return render(request, 'portfolio/project_detail.html', {'projects': projects})
