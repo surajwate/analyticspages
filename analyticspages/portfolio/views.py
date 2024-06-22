bc8d85b01eb88947b7b83278d27a7586138a656a
Suraj Wate <suraj.wate@gmail.com>
2024-06-22 20:45:54 +0100

    update views

--- analyticspages/portfolio/views.py
+++ analyticspages/portfolio/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Project
 
-def project_list(request):
-    projects = Project.objects.all()
+def project_list(request, pk):
+    projects = get_object_or_404(Project, pk=pk)
     return render(request, 'portfolio/project_list.html', {'projects': projects})
