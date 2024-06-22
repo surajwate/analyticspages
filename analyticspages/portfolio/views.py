058260f0d6f7ff01ddeb133e728d979af833ac11
Suraj Wate <suraj.wate@gmail.com>
2024-06-22 21:27:17 +0100

    made correction in views.py

--- analyticspages/portfolio/views.py
+++ analyticspages/portfolio/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Project
 
+def project_list(request):
+    projects = Project.objects.all()
+    return render(request, 'portfolio/project_list.html', {'projects': projects})
+
 def project_list(request, pk):
     projects = get_object_or_404(Project, pk=pk)
-    return render(request, 'portfolio/project_list.html', {'projects': projects})
+    return render(request, 'portfolio/project_detail.html', {'projects': projects})
