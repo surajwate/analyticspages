6a23403a0376d0f9972c47074158121f26faec9b
Suraj Wate <suraj.wate@gmail.com>
2024-06-22 16:08:28 +0100

    Create Test for the Model

--- analyticspages/portfolio/tests.py
+++ analyticspages/portfolio/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import pytest
+from .models import Project
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_project_create():
+    project = Project.objects.create(
+        title = 'Test Project',
+        description = 'Test Description',
+        technology = 'Test Technology',
+        image = 'images/test.jpg'
+    )
+    assert project.title == 'Test Project'
+    assert project.description == 'Test Description'
+    assert project.technology == 'Test Technology'
