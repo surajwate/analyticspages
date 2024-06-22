8f4ab2a80ee62dfff985c603281136ac8a55b210
Suraj Wate <suraj.wate@gmail.com>
2024-06-22 15:58:23 +0100

    Create model and apply migrations

--- analyticspages/portfolio/models.py
+++ analyticspages/portfolio/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    technology = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
