145779a5130fe5c46073861b87edc82a569bce8d
Suraj Wate <suraj.wate@gmail.com>
2024-06-22 16:10:49 +0100

    Create test for the view

--- analyticspages/portfolio/tests.py
+++ analyticspages/portfolio/tests.py
@@ -1,5 +1,6 @@
 import pytest
 from .models import Project
+from django.urls import reverse
 
 @pytest.mark.django_db
 def test_project_create():
@@ -12,3 +13,10 @@ def test_project_create():
     assert project.title == 'Test Project'
     assert project.description == 'Test Description'
     assert project.technology == 'Test Technology'
+
+@pytest.mark.django_db
+def test_project_list_view(client):
+    url = reverse('project_list')
+    response = client.get(url)
+    assert response.status_code == 200
+    assert 'Projects' in response.content.decode()
