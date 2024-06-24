13c634d94f043892bf8205ca266428ace137c182
Suraj Wate <suraj.wate@gmail.com>
2024-06-24 20:31:14 +0100

    updated docker-compose and settings

--- analyticspages/analyticspages/settings.py
+++ analyticspages/analyticspages/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'analyticspages.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'analyticspages',
-        'USER': 'suraj',
-        'PASSWORD': 'mypassword',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
