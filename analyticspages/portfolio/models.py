d29372089ba7266475d0b3cbbfac09a2418a5169
Suraj Wate <suraj.wate@gmail.com>
2024-06-24 15:49:34 +0100

    Made changes in models because test was failing

--- analyticspages/portfolio/models.py
+++ analyticspages/portfolio/models.py
@@ -5,7 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     technology = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
     start_date = models.DateField(default=timezone.now)
     end_date = models.DateField(null=True, blank=True)
     github_link = models.URLField(max_length=200, null=True, blank=True)
