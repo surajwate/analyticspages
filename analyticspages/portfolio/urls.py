68ca9544b8158c10587b5708e5c639a6a8e20504
Suraj Wate <suraj.wate@gmail.com>
2024-06-24 14:48:07 +0100

    Updated URL routing for the new project create & update views

--- analyticspages/portfolio/urls.py
+++ analyticspages/portfolio/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CustomLoginView, CustomLogoutView, RegisterView, ProfileView
+from .views import CustomLoginView, CustomLogoutView, RegisterView, ProfileView, project_create, project_update
 from . import views
 
 urlpatterns = [
@@ -7,6 +7,8 @@ urlpatterns = [
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/', ProfileView.as_view(), name='profile'),
+    path('create/', project_create, name='project_create'),
+    path('update/<int:pk>/', project_update, name='project_update'),
     path('', views.project_list, name='project_list'),
     path('project/<int:pk>/', views.project_detail, name='project_detail'),
 ]
