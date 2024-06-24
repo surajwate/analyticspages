92aca5b169af7bb1d3c6e6abd514263c2c23a217
Suraj Wate <suraj.wate@gmail.com>
2024-06-24 12:09:51 +0100

    Update test
    Add tests for the authentication views
    
    - There is a warning about `GET` requests for logout in future.
    From Django 5 `POST` request from logout will be required.

--- analyticspages/portfolio/tests.py
+++ analyticspages/portfolio/tests.py
@@ -3,6 +3,7 @@ from .models import Project
 from django.urls import reverse
 from django.contrib.admin.sites import AdminSite
 from .admin import ProjectAdmin
+from django.contrib.auth.models import User
 
 # Test for creating a project instance and verifying its attributes
 @pytest.mark.django_db
@@ -81,3 +82,38 @@ def project():
         technology='Test Technology',
         image='images/housing.jpg'  # Ensure the image file exists in the 'media/images' directory of the project
     )
+
+@pytest.mark.django_db
+def test_register(client):
+    url = reverse('register')
+    response  = client.get(url)
+    assert response.status_code == 200
+    data = {
+        'username': 'testuser',
+        'password1': 'complexpassword123',
+        'password2': 'complexpassword123'
+    }
+    response = client.post(url, data)
+    assert response.status_code == 302 # Redirect after successful registration
+    assert User.objects.filter(username='testuser').exists() # Check if the user was created successfully
+
+@pytest.mark.django_db
+def test_login(client):
+    User.objects.create_user(username='testuser', password='complexpassword123')
+    url = reverse('login')
+    response = client.get(url)
+    assert response.status_code == 200
+    data = {
+        'username': 'testuser',
+        'password': 'complexpassword123'
+    }
+    response = client.post(url, data)
+    assert response.status_code == 302
+
+@pytest.mark.django_db
+def test_logout(client, django_user_model):
+    testuser = django_user_model.objects.create_user(username='testuser', password='complexpassword123')
+    client.login(username='testuser', password='complexpassword123')
+    url = reverse('logout')
+    response = client.get(url)
+    assert response.status_code == 302
