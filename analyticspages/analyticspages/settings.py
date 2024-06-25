2bcf832552d1209c192ce5f7a8a3cd435166475c
Suraj Wate <suraj.wate@gmail.com>
2024-06-25 22:41:55 +0100

    somec

--- analyticspages/analyticspages/settings.py
+++ analyticspages/analyticspages/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.getenv('DEBUG', 'True') == True
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == True
+# DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
